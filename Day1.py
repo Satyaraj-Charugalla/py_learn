2987968ab5ad61648237078785096eba4b5b47c4
Satyaraj-Charugalla <1210316950@gitam.in>
2022-08-29 16:12:13 +0530

    Looping

--- Day1.py
+++ Day1.py
@@ -1,5 +1,13 @@
 #print ("Hello")
 
-for i in range(6):
-    x= i * 2;
-    print(x)
+#for i in range(6):
+#    x= i * 2;
+#    print(x)
+
+x = int(input("Enter a value: "))
+if x == 200:
+    print("Value is equal")
+elif x > 200:
+    print("Value greater than 200")
+else:
+    print("Enter a value or the value enterd is < 200")
