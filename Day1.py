dcf0ec8a91e8151c747cc10829169cbfe83097c7
Satyaraj-Charugalla <1210316950@gitam.in>
2022-08-29 16:26:55 +0530

    Loops

--- Day1.py
+++ Day1.py
@@ -4,10 +4,25 @@
 #    x= i * 2;
 #    print(x)
 
-x = int(input("Enter a value: "))
-if x == 200:
-    print("Value is equal")
-elif x > 200:
-    print("Value greater than 200")
-else:
-    print("Enter a value or the value enterd is < 200")
+#----------------Loop--------------
+#x = int(input("Enter a value: "))
+#if x == 200:
+#    print("Value is equal")
+#elif x > 200:
+#    print("Value greater than 200")
+#else:
+#    print("Enter a value or the value enterd is < 200")
+#-----------------------------------
+
+#-----------While Loop--------------
+"""
+inn = int(input("Enter a value: "))
+while True:
+    if inn % 7 ==0:
+        print(inn)
+        break
+    else:
+        print("Value not divisible by 7")
+        break
+"""
+print("Hallow!")
