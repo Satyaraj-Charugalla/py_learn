5bd4c996c7bd34526e67b80702deb3ecfd65cc5b
Satyaraj-Charugalla <1210316950@gitam.in>
2022-09-01 18:17:20 +0530

    The final push for Day 3

--- Day3.py
+++ Day3.py
@@ -1,5 +1,6 @@
 #This will have all the code practices that are done in day 2
 #----------------------Collection Types----------------------
+"""
 from urllib.request import urlopen
 story = urlopen('http://sixty-north.com/c/t.txt')
 story_words = []
@@ -11,3 +12,30 @@ story.close()
 
 for words in story_words:
     print(words)
+
+"""
+
+#Strings
+#b = "Hello, World!"
+#print(b[-5:-2])
+#print(b[2:])
+#print(b[:4])
+#print(b.split(","))  # this is the split function
+
+#------------------Format Function-------------------
+#age = 36
+#txt = "My name is John, and I am {}"
+#print(txt.format(age))
+
+#You can use index numbers {0} to be sure the arguments are placed in the correct placeholders
+"""
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
+"""
+
+#The escape character allows you to use double quotes when you normally would not be allowed:
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
