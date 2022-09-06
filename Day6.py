8c42dda3c059ec5b7b114b049899645ea353506f
Satyaraj-Charugalla <1210316950@gitam.in>
2022-09-06 18:27:11 +0530

    Object and Types

--- Day6.py
+++ Day6.py
@@ -1,4 +1,7 @@
 """
+#The main objective is to show that the variables are immutebale
+#which means that if a variable is binded with a value and later it is updated the 
+#variable will be refering to the new value and the old value will be trashed...Not replaced
 x = 10
 y = x
 x = 25
