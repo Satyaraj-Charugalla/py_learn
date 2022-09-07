d3168b3c25c038e09f8948913a8385e12fdd2313
Satyaraj-Charugalla <1210316950@gitam.in>
2022-09-07 17:30:26 +0530

    Day 7 scopes commit

---
+++ Day7.py
@@ -0,0 +1,13 @@
+#See how a global name is not being updated
+
+glob = 'Hello' # Global variable
+def glob_fun():
+    print(glob) #Printing the variable
+
+def update_glob_fun1(c):
+    glob = c  #Trying to update using a pass by value
+
+glob_fun()
+print(update_glob_fun1(20)) 
+#It will not update since the global name cannot be rebinded to another value
+glob_fun()
