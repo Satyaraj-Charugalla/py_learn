75fd8e2f609cd1ff744f0998a6512c9c12a25246
Satyaraj-Charugalla <1210316950@gitam.in>
2022-09-08 18:28:34 +0530

    Day 8 commits about list, range, strings

---
+++ Day8_2.py
@@ -0,0 +1,21 @@
+#------------------------------Dictionaries--------------------------------
+# The dictionary hold the values in pairs
+# Syntax => {'key': value}
+# Keys can be anything execpt list or tuples as they are muttable; values can be anything
+"""
+name_age = (['Alice',20],['Banhi',21],['Doodle',12])
+name_age_dicto = dict(name_age)
+print(name_age_dicto)
+
+alpa = dict(a = 'Alpha', b='Beta')
+print(alpa)
+
+
+d = dict(goldenroad = 0xDAA520, indigo = 0x4B0082)
+e=d.copy()
+#print('This is d',d)
+#print(e)
+
+f = dict(wheat = 0xF5DEB3,Khaki = 0xF0E68C)
+print(e.update(f))
+"""
