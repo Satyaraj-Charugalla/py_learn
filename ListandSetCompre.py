b700f0504a99e3484a91f08e4cf73d1c51d4b00e
Satyaraj-Charugalla <1210316950@gitam.in>
2022-09-13 15:57:28 +0530

    Comprehensions

--- ListandSetCompre.py
+++ ListandSetCompre.py
@@ -1,8 +1,19 @@
 #-----------------------Lists and set comprehensions-----------------
+"""
 words = 'This is some randomly generated text for knowing what is the List and set comprehensions'.split()
 print(words)
+"""
 
-
-# Syntax for the comprehensions is 
+# Syntax for the list comprehensions is 
 # [expr(item) for item in iterable] Iterable is the variable that holds value
-print([len(word) for word in words])
+# print([len(word) for word in words])
+
+from math import sqrt
+def is_prime(x):
+    if x < 2:
+        return False
+    for i in range(2,int(sqrt(x)) +1):
+        if x % i == 0:
+            return False
+        return i
+print ([x for x in range(101) if is_prime(x)])
