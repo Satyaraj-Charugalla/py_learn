5b602f2e2c500082b73aa1acfad0efae192d32e6
Satyaraj-Charugalla <1210316950@gitam.in>
2022-09-14 17:32:02 +0530

    Classes

--- Classes.py
+++ Classes.py
@@ -6,9 +6,11 @@ class Flights:
     pass
 """
 
+"""
 class marks:
-    def __init__(aaa,m1,m2,m3):   #the init is called as 
-        #constructor and is used to bind the parameters to the arguments that are passed
+    def __init__(aaa,m1,m2,m3):   
+        # the init is called as Initialiser and is used to bind the parameters 
+        # to the arguments that are passed
         # aaa here will bind the arguments to the parameters in method
         aaa.m1 = m1
         aaa.m2 = m2
@@ -20,10 +22,10 @@ class marks:
     def sum(aaa):
         return aaa.m1+aaa.m2+aaa.m3
 
-
 stud1 = marks(20,40,60)
 stud2 = marks(80,10,60)
 
 print (f'The avergae of the 2 students is {(stud1.sum()+stud2.sum())/2}')
 #print (f'The sum of the marks is {stud1.avg()}')
 #print (f'The sum of the marks is {stud1.sum()}')
+"""
