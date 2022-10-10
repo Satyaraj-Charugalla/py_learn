ca94a9572379b70ba5f782f7c2b3e0e196b2f937
Satyaraj-Charugalla <1210316950@gitam.in>
2022-10-10 14:40:45 +0530

    Coursera python

--- random10/10/2022.py
+++ random10/10/2022.py
@@ -1,6 +1,7 @@
 #Loops
 largest = 0
-for i in [23,11,211,433,22,3,22]:
+num_lis = [23,11,211,433,22,3,22]
+for i in num_lis:
     if i > largest:
         largest = i
 print('The largest in the list', largest)
