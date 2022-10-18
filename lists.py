f060662cb74c92b1478e1f206395338e5edab5ed
Satyaraj-Charugalla <1210316950@gitam.in>
2022-10-18 11:32:27 +0530

    Commit

---
+++ lists.py
@@ -0,0 +1,36 @@
+"""
+#print(range(5))
+fri = ['AA','BB','CC']
+#print(len(fri))
+#print(range(len(fri)))
+print(fri)
+fri[2] = 'DD'
+print(fri)
+"""
+
+"""
+nums =[1,2,3,4,5,6]
+nums1 = [77,8,66,74]
+nums2 = nums+nums1
+print(nums2)
+print(nums[:3])
+"""
+
+"""
+nums =[1,2,3,4,5,6]
+#nums.append(22)
+#nums.append(300)
+nums.append(600,3333) # will not work
+print(nums)
+"""
+
+"""
+num = [23,423,5535,243,134,11,44]
+num.sort(reverse=False)
+# If reverse is false then the list will be sorted in a Ascending order
+print(num)
+"""
+
+friends = [ 'Joseph', 'Glenn', 'Sally' ]
+friends.sort()
+print(friends[0])
