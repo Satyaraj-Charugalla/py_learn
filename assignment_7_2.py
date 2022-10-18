bf470730a01d74624860e25948ef6b6629c11885
Satyaraj-Charugalla <1210316950@gitam.in>
2022-10-18 09:58:10 +0530

    Added some comments to the assignment_7_2.py file

--- assignment_7_2.py
+++ assignment_7_2.py
@@ -5,8 +5,11 @@ fh = open(fname)
 for line in fh:
     if line.startswith("X-DSPAM-Confidence:"):
         count = count+1
-        striping = line.rstrip()
-        x = striping.find(':')
+        striping = line.rstrip() 
+        #A rstrip is used for removing the extra spacing at the end of the line
+        x = striping.find(':') 
+        #Since the requirment is to find the number and before the number there's a :
         val = float(line[x+2:-1])
+        #There is a space after the colon so we have added 2 for the position of the value x
         tot_val = tot_val + val
 print(f'Average spam confidence: {tot_val/count}')
