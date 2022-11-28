9768c1ef070e6581204c313e7e5de564e880ebfd
Satyaraj-Charugalla <1210316950@gitam.in>
2022-11-28 12:43:52 +0530

    Round 2 of sockets

--- sockets.py
+++ sockets.py
@@ -1,4 +1,6 @@
 import socket 
+#ip = socket.gethostbyname('www.google.com')
+#print(ip)
 
-ip = socket.gethostbyname('www.google.com')
-print(ip)
+#mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#mysock.connect(('data.pr4e,org',80))
