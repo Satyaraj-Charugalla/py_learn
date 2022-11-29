b13aee5dda688c96e64224a107c49caa9ef59642
Satyaraj-Charugalla <1210316950@gitam.in>
2022-11-29 16:43:15 +0530

    Added comments for the socket.py file

--- sockets.py
+++ sockets.py
@@ -7,13 +7,16 @@ import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org',80))
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode() 
+# The encode will update the string into the bytes format.
+# Here the string we are sending over is the string inside cmd object.
 mysock.send(cmd)
 
 while True:
-    data = mysock.recv(512)
+    data = mysock.recv(512) # The data we recive wil be in bytes
     if(len(data) < 1):
         break
-    print(data.decode())
+    print(data.decode()) # Here the decode will be changing the bytes data into the string data.
 
 mysock.close()
+# The socket connection must be closed.
