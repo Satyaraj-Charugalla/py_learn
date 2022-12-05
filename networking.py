754fe73096da516cb53eece21b7f4b4ff704dbee
Satyaraj-Charugalla <1210316950@gitam.in>
2022-12-05 13:36:46 +0530

    Tried urllib but it is not working

--- networking.py
+++ networking.py
@@ -1,9 +1,19 @@
-import urllib.request, urllib.parse, urllib.error
-
+import urllib.request
+"""
 fhand = urllib.request.urlopen('http://py4e.org/romeo.txt')
 count = 0
 for line_count in fhand:
     print(line_count.decode().strip())
     count = count +1
+
+print(count)
+"""
+
+with urllib.request.urlopen('http://py4e.org/romeo.txt') as response:
+    fhand = response.read()
+    count = 0
+    for line_count in fhand:
+        print(line_count.decode().strip())
+        count = count +1
 
 print(count)
