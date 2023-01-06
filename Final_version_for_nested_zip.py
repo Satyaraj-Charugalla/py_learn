5d671b3136504e365e0ff1d9f1e3cc3450ee592b
Satyaraj-Charugalla <1210316950@gitam.in>
2023-01-06 16:12:26 +0530

    Extract year

--- Final_version_for_nested_zip.py
+++ Final_version_for_nested_zip.py
@@ -2,7 +2,7 @@ from zipfile import ZipFile
 from pathlib import Path
 import os
 
-main_zip = "C:\\Users\\scharuga\\Downloads\\Testing_Zip.zip"
+main_zip = "C:\\Users\\scharuga\\Downloads\\Testing_Nested_Zip.zip"
 store_zip_extarct = "C:\\Users\\scharuga\\Downloads\\Unpack_Files"
 with ZipFile(main_zip, 'r') as zObject:
     zObject.extractall(path = store_zip_extarct)
@@ -12,7 +12,7 @@ direct_list = []
 for dirctore in Path(store_zip_extarct).glob("*.zip"):
     x = str(dirctore)
     direct_list.append(x)
-print(f'Files location:\n{direct_list}')
+#print(f'Files location:\n{direct_list}')
 
 list_len = len(direct_list)
 if list_len > 0:
@@ -23,17 +23,17 @@ if list_len > 0:
         #print(f"The position of the \\ for path {x} is at {z}")
         to_string = str(x[z+1:-4])
         nested_zip_files_names.append(to_string)
-    print(f'Zip file names in the location:\n{nested_zip_files_names}')
+    #print(f'Zip file names in the location:\n{nested_zip_files_names}')
 
     #create the sub directories
     nested_zip_files_directories= []
-    mode = 0o666
+    #mode = 0o666
     for sub_directory_names in nested_zip_files_names:
         Path = os.path.join(store_zip_extarct, sub_directory_names)
-        os.mkdir(Path,mode)
+        os.mkdir(Path)
         st = str(Path)
         nested_zip_files_directories.append(st)
-    print(f'Sub directory names:\n{nested_zip_files_directories}')
+    #print(f'Sub directory names:\n{nested_zip_files_directories}')
 
     #Extracting the zip files data
     for zip_list in direct_list:
---
+++ present_year.py
@@ -0,0 +1,4 @@
+import datetime
+present_date = datetime.date.today()
+present_year = present_date.year
+print(present_year)
