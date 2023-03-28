3109f61c2694e954ce1e3059257fb7fc1b364cb2
Satyaraj-Charugalla <1210316950@gitam.in>
2023-03-28 17:55:11 +0530

    Learnt a bit about classes and a new module UUID

---
+++ using_classes.py
@@ -0,0 +1,18 @@
+# We are creating a clas named foodie
+class foodie:
+    # A constructor (__init__) is used to assign the values
+    def __init__(self,colur,fruitname):
+        self.colur = colur
+        self.fruitname= fruitname
+    # A function that is defined in a class is called a method
+    # The method here will print out the name and colour of the fruit
+    def show(self):
+    # The word self is an instance to access all the attributes in the class
+        print(f'Fruit name is {self.fruitname}')
+        print(f'Fruit colour is {self.colur}')
+
+fru_mango = foodie('Yellow','Mango')
+fru_avacado = foodie('Green','Avacado')
+
+fru_mango.show()
+fru_avacado.show()
