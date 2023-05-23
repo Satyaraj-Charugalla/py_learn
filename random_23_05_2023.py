5522361cad0b51e4d14d02ca4a33f5e9aa4c5a2b
Satyaraj-Charugalla <1210316950@gitam.in>
2023-05-23 14:53:37 +0530

    Random file upload

---
+++ random_23_05_2023.py
@@ -0,0 +1,10 @@
+"""
+import datetime
+daterange = datetime.date.today()
+#print(daterange)
+current_year = daterange.year
+print(current_year)
+"""
+
+import sys
+print('Hi there! This is {}'.format(sys.argv[0]))
